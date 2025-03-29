a42bc8b3ca59d6e4a2aa2b52812ce6fcd5d7fa10
ediazm <alcmaeon@gmail.com>
2025-03-29 15:18:07 -0700

    perhaps

--- app.config.ts
+++ app.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
     build: {
       // Ensure assets are placed in the correct directory
       assetsDir: 'assets',
+      rollupOptions:{
+        output: {
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        }
+      }
     },
   },
 })
