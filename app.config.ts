eac299e4879c6fb19c7ec915b2526718d5d9b033
ediazm <alcmaeon@gmail.com>
2025-03-31 21:10:01 -0700

    test13

--- app.config.ts
+++ app.config.ts
@@ -9,18 +9,5 @@ export default defineConfig({
         projects: ['./tsconfig.json'],
       }),
     ],
-    build: {
-      // Ensure assets are placed in the correct directory
-      manifest: true,
-      assetsDir: '_build/assets',
-      rollupOptions: {
-        output: {
-          assetFileNames: ({ name }) => {
-            const base = name ? path.parse(name).name : 'asset'
-            return `_build/assets/${base}-[hash][extname]`
-          },
-        },
-      },
-    },
   },
 })
